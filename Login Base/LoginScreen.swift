8e758825ee9fefb44866b345a6cceaaaca9fc44d
MrRobot420 <max.karlchen@web.de>
2018-03-14 01:49:45 +0100

    - Added LoginScreen - ViewController - Removed Login Screen from first page

---
+++ Login Base/LoginScreen.swift
@@ -0,0 +1,26 @@
+//
+//  LoginScreen.swift
+//  Login Base
+//
+//  Created by Maximilian Karl on 14.03.18.
+//  Copyright © 2018 Robot. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class LoginScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}

a1d46dc91fb7664b0a714f33d8b48665f6fb4b48
Johannes Velde <jo@johanness-mbp.fritz.box>
2018-03-14 02:06:24 +0100

    RegistrationViewController added

---
+++ Login Base/RegistrationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RegistrationViewController.swift
+//  Login Base
+//
+//  Created by Johannes Velde on 14.03.18.
+//  Copyright © 2018 Robot. All rights reserved.
+//
+
+import Foundation
+
+class Registraton : UIViewController {
+    
+}
